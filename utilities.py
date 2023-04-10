2c2be9e3bb837cc375ea6525eb38749073ec7187
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-11 02:51:48 +0300

    create utilities class

---
+++ utilities.py
@@ -0,0 +1,8 @@
+
+def get_valid_input(input_question : str, valid_options : tuple):
+    input_question += " ({}) ".format(", ".join(valid_options))
+    response = input(input_question)
+    while response.lower().strip() not in valid_options:
+        print("Please type into valid options...!")
+        response = input(input_question)        
+    return response
