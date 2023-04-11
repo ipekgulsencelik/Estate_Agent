64761e9560db27100b9c67d47a5e102910cfbf15
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-11 03:30:47 +0300

    create rental class

---
+++ rental.py
@@ -0,0 +1,26 @@
+from property import Property
+from utilities import get_valid_input
+
+class Rental(Property):
+    def __init__(self, furnished='', extras='', rent='', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.furnished = furnished
+        self.rent = rent
+        self.extras = extras
+
+    def display(self):
+        super().display()
+        print(f'RENTAL DETAILS\n'
+              f'Rent: {self.rent}\n'
+              f'Furnished: {self.furnished}\n'
+              f'Extras: {self.extras}\n'
+        )
+
+    def prompt_init():
+        return dict(
+            furnished= get_valid_input("Has property furnished? ",("yes", "no")),
+            extras = input("What are the extras prefer? "),
+            rent = input("What is the established monthly rent? "),
+        )
+    
+    prompt_init = staticmethod(prompt_init)
