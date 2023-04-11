9968c1df108df565b1b43b0fa3cf1d3471f87d0f
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-11 03:58:56 +0300

    adding apartment purchase class

--- purchase.py
+++ purchase.py
@@ -23,3 +23,13 @@ class Purchase(Property):
         )
     
     prompt_init = staticmethod(prompt_init)
+
+
+class ApartmentPurchase(Apartment, Purchase):
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Purchase.prompt_init())
+
+        return init
+    
+    prompt_init = staticmethod(prompt_init)
